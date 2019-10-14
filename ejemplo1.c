b6ef87c575b14541be7e139fb01e234c7faf76ab
HENRYTONY5 <55508960+HENRYTONY5@users.noreply.github.com>
2019-10-14 15:57:28 -0500

    Add files via upload

---
+++ ejemplo1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+void imp();
+
+ void leer_o(int& o);
+
+int main(){
+
+int a,b,c;
+int o;
+imp();
+int leer(o);
+
+}
+void imp(){
+
+printf("\n  1 para suma \n");
+printf("  2 para resta \n");
+printf("  3 para multiplicacion \n");
+printf("  4 para division \n");
+
+}
+void leer_o(int& o){
+
+   printf("introduce opcion\n");
+   scanf("%d",&o);
+}
